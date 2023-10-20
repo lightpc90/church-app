6824d8441230ec23e5c2fb97268d403719952483
lightpc90 <afoageektech@gmail.com>
2023-10-20 14:06:27 +0100

    included icons in mobile nav

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,17 +1,40 @@
 import React from 'react'
 
+import { AiTwotoneHome } from 'react-icons/ai'
+import { ImProfile } from 'react-icons/im'
+import { MdEventRepeat, MdRoundaboutRight } from "react-icons/md";
+import { FaBlogger } from 'react-icons/fa'
+import { RiEmpathizeFill } from 'react-icons/ri'
+import {BsRocketTakeoffFill} from 'react-icons/bs'
+
+
 const MobileNav = () => {
   return (
-    <div className='fixed top-[80px] left-0 flex flex-col bg-slate-950 text-white px-5 pt-5 w-screen min-h-screen z-50 overflow-auto'>
-      <ul className='divide-y'>
-        <li>Home</li>
-        <li>My Profile</li>
-        <li>About Us</li>
-        <li>Events</li>
-        <li>Blog</li>
-        <li>Counselling</li>
-        <li>Youth Forum</li>
-    </ul>
+    <div className="fixed top-[80px] left-0 flex flex-col bg-slate-950 text-white px-5 pt-5 w-screen min-h-screen z-50 overflow-auto">
+      <ul className="divide-y">
+        <li className="flex gap-2 pt-5">
+          <span>
+            <AiTwotoneHome />
+          </span>
+          Home
+        </li>
+        <li className="flex gap-2 pt-5">
+          <span>
+            <ImProfile />
+          </span>
+          My Profile
+        </li>
+        <li className="flex gap-2 pt-5">
+          <span>
+            <MdRoundaboutRight />
+          </span>
+          About Us
+        </li>
+        <li className="flex gap-2 pt-5"><span><MdEventRepeat/></span>Events</li>
+        <li className="flex gap-2 pt-5"><span><FaBlogger/></span>Blog</li>
+        <li className="flex gap-2 pt-5"><span><RiEmpathizeFill/></span>Counselling</li>
+        <li className="flex gap-2 pt-5"><span><BsRocketTakeoffFill/></span>Youth Forum</li>
+      </ul>
     </div>
   );
 }
