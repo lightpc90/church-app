d3110796db5130099f80165ec5791eaf98fcbf96
lightpc90 <afoageektech@gmail.com>
2023-10-24 20:19:15 +0100

    added registration section in the admin dashboard

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,20 +1,31 @@
 import Profile from "@/components/user/profile"
 import Searchworkers from "@/components/admin/Searchworkers"
+import RegisterForm from "@/components/RegisterForm";
 
 const Page = () => {
     return (
       <div className="mt-[80px] mx-[20px] md:mx-[70px]">
         <h4>Admin Dashboard</h4>
+
         {/* profile section card*/}
         <section className="bg-[#060633] px-10 rounded-xl shadow-xl text-white">
           <Profile />
         </section>
+
         {/* search workers */}
-        <section className="flex flex-col justify-center items-center">
+        <section className="flex flex-col justify-center items-center my-2">
           <h6>Manage Users</h6>
-          <section className="bg-[#1e3a8a] mb-2 p-5 rounded-xl shadow-xl text-white w-full">
+          <div className="bg-[#1e3a8a] mb-2 p-5 rounded-xl shadow-xl text-white w-full">
             <Searchworkers />
-          </section>
+          </div>
+        </section>
+
+        {/* Register a User */}
+        <section className="flex flex-col justify-center items-center my-2">
+                <h6>Register a User</h6>
+                <div className="bg-[#2e074b] p-5 rounded-xl shadow-xl text-white w-full">
+                    <RegisterForm/>
+                </div>
         </section>
       </div>
     );
