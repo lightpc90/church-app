f397a0f44f13634b15480f7d78fe4ffee2ec79e7
lightpc90 <afoageektech@gmail.com>
2023-11-07 22:53:35 +0100

    align the pastor layout in about us

--- src/components/PastorLayout.js
+++ src/components/PastorLayout.js
@@ -4,14 +4,14 @@ import Avatar from "./avatar";
 
 export const PastorLayout = ({ dp, name, social }) => {
   return (
-    <div className=" w-[45%] md:w-[30%]">
+    <div className="flex flex-col items-center w-[45%] md:w-[30%]">
       {/* Avatar */}
-      <div className="">
+      <div className="w-full">
         <Avatar dp={dp} />
       </div>
 
-      <p>{name}</p>
-      <p className='text-sm'>{social}</p>
+      <p className='w-full'>{name}</p>
+      <p className='text-sm w-full'>{social}</p>
     </div>
   );
 };
