d6e855457efcacaea576cf575d79a503470e12a3
lightpc <afoageektech@gmail.com>
2024-08-24 01:56:03 +0100

    "Fixed apostrophe in 'God's' in page title"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
         <div className="flex items-center lg:p-20 bg-gradient-to-t from-zinc-800 via-slate-800 opacity-90 w-full h-full absolute z-30">
           <div className="flex flex-col text-white gap-5 font-bold lg:w-5/12 lg:pt-[50px]">
             <p className="lg:text-[50px]   ">
-              Experience Gods Love through worship and fellowship at our Church.{" "}
+              Experience God&apos;s Love through worship and fellowship at our Church.{" "}
             </p>
             <p className="lg:text-2xl">
               Experience a welcoming community and uplifting sermons at our
