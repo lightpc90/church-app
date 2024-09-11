f80f3ccefec68d49502725b3ee4f1a63a6f04f55
lightpc <afoageektech@gmail.com>
2024-09-11 07:48:56 +0100

    "Updated Footer component: changed vision section title and content, added Button and Link imports, and added a link to 'Meet Our Pastor'"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ import SocialMedia from "@/components/social-media/SocialMedia";
 import LiveStream from "@/components/livestream/LiveStream";
 import LiveProgram from "@/components/livestream/LiveProgram";
 import MapComponent from "@/components/map/GoogleMap";
-import InstagramFeed from "@/components/instagramFeed/InstagramFeed"
+import InstagramFeed from "@/components/instagramFeed/InstagramFeed";
 
 const Items = [
   banner1.src,
@@ -51,21 +51,21 @@ export default function Home() {
           className="flex items-end p-5 lg:p-20 bg-gradient-to-t from-zinc-800 via-slate-800 
         opacity-90 absolute top-0 left-0 w-[100vw] h-full z-30"
         >
-          <div className="flex flex-col text-white gap-1 lg:gap-5 font-bold w-10/12 lg:w-5/12 pt-[30px] lg:pt-[50px]">
-            <h2 className="text-md  lg:text-[50px]  ">
+          <div className="flex flex-col text-white gap-1 lg:gap-5 font-bold w-10/12 lg:w-5/12 pt-[30px]">
+            <h2 className=" lg:text-[50px]  ">
               Experience God&apos;s Love through worship and fellowship at our
               Church.
             </h2>
-            <p className="text-sm lg:text-2xl text-zinc-400">
+            <p className="lg:text-2xl text-zinc-400">
               Experience a welcoming community and uplifting sermons at our
               church. All are welcome. Visit us and reach out with any question
             </p>
             {/* buttons */}
-            <div className="flex flex-wrap max-w-[400px] gap-2  lg:gap-4 mt-5">
-              <button className="bg-rose-800 w-[150px] p-2 lg:py-3 lg:px-4 rounded-md">
+            <div className="flex flex-wrap max-w-[400px] lg:max-w-[700px] gap-2  lg:gap-4 mt-5">
+              <button className="bg-rose-800 w-[150px] lg:w-fit  p-2 lg:py-3 lg:px-4 rounded-md">
                 Connect with Us
               </button>
-              <button className="border w-[150px] p-2 lg:py-3  lg:px-4 rounded-md">
+              <button className="border w-[150px] lg:w-fit p-2 lg:py-3  lg:px-4 rounded-md">
                 Learn More
               </button>
             </div>
@@ -74,134 +74,140 @@ export default function Home() {
       </section>
 
       {/* contents */}
-      <div className="flex flex-col lg:p-[50px]">
+      <div className="flex flex-col lg:p-[50px] items-center">
         {/* welcome */}
-        <div className="flex flex-col gap-4 p-[20px] items-center justify-center ">
-          <div className="text-center">
-            <h1 className="text-2xl text-center font-bold ">
-              {ChurchWelcomeParagraph.title}
-            </h1>
-            <h4 className="font-bold text-slate-400">
-              {ChurchWelcomeParagraph.description}
-            </h4>
+        <div className="flex flex-wrap gap-4 p-[20px] items-center justify-center my-10">
+          <div className="w-full lg:w-[30%]">
+            <div className="text-center">
+              <h1 className="text-2xl text-center font-bold ">
+                {ChurchWelcomeParagraph.title}
+              </h1>
+              <h4 className="font-bold text-slate-400">
+                {ChurchWelcomeParagraph.description}
+              </h4>
+            </div>
+            <p className="text-justify ">{ChurchWelcomeParagraph.paragraph}</p>
           </div>
-          <p className="text-justify ">{ChurchWelcomeParagraph.paragraph}</p>
-          <div className="w-full h-[300px] bg-slate-300">
+
+          <div className="w-full lg:w-[30%] h-[400px] bg-slate-300">
             {/* Pastor Picture coming here */}
+            hhhhh
           </div>
         </div>
 
         {/* CTA Cards */}
-        <div className="flex flex-wrap gap-4 w-full justify-center items-center">
+        <div className="flex flex-col gap-4 w-full lg:w-[70%] justify-center items-center">
           <h1 className="text-2xl font-bold text-slate-800">
             What&apos;s Next?
           </h1>
-          {/* card 1 */}
-          <div className="flex flex-col justify-evenly w-[300px] h-[310px] bg-rose-800 rounded-2xl shadow-lg p-4 text-white">
-            {/* title */}
-            <div className="flex gap-4 text-2xl items-center">
-              {/* title icon */}
-              <span>
-                <FaCross size={70} />
-              </span>
-              <h2 className="font-bold">How Can I Be Saved?</h2>
+          <div className="flex flex-wrap gap-5 w-full lg:w-[90%] items-center justify-center">
+            {/* card 1 */}
+            <div className="flex flex-col justify-evenly w-[300px] h-[310px] bg-rose-800 rounded-2xl shadow-lg p-4 text-white">
+              {/* title */}
+              <div className="flex gap-4 text-2xl items-center">
+                {/* title icon */}
+                <span>
+                  <FaCross size={70} />
+                </span>
+                <h2 className="font-bold">How Can I Be Saved?</h2>
+              </div>
+              {/* content */}
+              <p className="font-bold text-zinc-400">
+                The most important question you will ever ask that will affect
+                you for eternity
+              </p>
+              {/* call to action */}
+              <button className="border py-2 px-4 rounded-full w-fit ">
+                Learn More
+              </button>
             </div>
-            {/* content */}
-            <p className="font-bold text-zinc-400">
-              The most important question you will ever ask that will affect you
-              for eternity
-            </p>
-            {/* call to action */}
-            <button className="border py-2 px-4 rounded-full w-fit ">
-              Learn More
-            </button>
-          </div>
 
-          {/* card 2 */}
-          <div className="flex flex-col justify-evenly w-[300px] h-[310px] bg-slate-900 rounded-2xl shadow-lg p-4 text-white">
-            {/* title */}
-            <div className="flex gap-4 text-2xl items-center">
-              {/* title icon */}
-              <span>
-                <TfiVideoClapper size={70} />
-              </span>
-              <h2 className="font-bold">Watch Our Sermon & Services</h2>
+            {/* card 2 */}
+            <div className="flex flex-col justify-evenly w-[300px] h-[310px] bg-slate-900 rounded-2xl shadow-lg p-4 text-white">
+              {/* title */}
+              <div className="flex gap-4 text-2xl items-center">
+                {/* title icon */}
+                <span>
+                  <TfiVideoClapper size={70} />
+                </span>
+                <h2 className="font-bold">Watch Our Sermon & Services</h2>
+              </div>
+              {/* content */}
+              <p className="font-bold text-zinc-400">
+                Hear God&apos;s word delivered by our pastors in our sermon
+                archives
+              </p>
+              {/* call to action */}
+              <button className="border py-2 px-4 rounded-full w-fit ">
+                See Messages
+              </button>
             </div>
-            {/* content */}
-            <p className="font-bold text-zinc-400">
-              Hear God&apos;s word delivered by our pastors in our sermon
-              archives
-            </p>
-            {/* call to action */}
-            <button className="border py-2 px-4 rounded-full w-fit ">
-              See Messages
-            </button>
-          </div>
 
-          {/* card 3 */}
-          <div className="flex flex-col justify-evenly w-[300px] h-[310px] bg-green-900 rounded-2xl shadow-lg p-4 text-white">
-            {/* title */}
-            <div className="flex gap-4 text-2xl items-center">
-              {/* title icon */}
-              <span>
-                <LiaPrayingHandsSolid size={70} />
-              </span>
-              <h2 className="font-bold">Submit a Prayer Request</h2>
+            {/* card 3 */}
+            <div className="flex flex-col justify-evenly w-[300px] h-[310px] bg-green-900 rounded-2xl shadow-lg p-4 text-white">
+              {/* title */}
+              <div className="flex gap-4 text-2xl items-center">
+                {/* title icon */}
+                <span>
+                  <LiaPrayingHandsSolid size={70} />
+                </span>
+                <h2 className="font-bold">Submit a Prayer Request</h2>
+              </div>
+              {/* content */}
+              <p className="font-bold text-zinc-400">
+                Need Prayer? Submit your requests to pastor to pray with you
+              </p>
+              {/* call to action */}
+              <button className="border py-2 px-4 rounded-full w-fit ">
+                Request Prayer
+              </button>
             </div>
-            {/* content */}
-            <p className="font-bold text-zinc-400">
-              Need Prayer? Submit your requests to pastor to pray with you
-            </p>
-            {/* call to action */}
-            <button className="border py-2 px-4 rounded-full w-fit ">
-              Request Prayer
-            </button>
-          </div>
 
-          {/* card 4 */}
-          <div className="flex flex-col justify-evenly w-[300px] h-[310px] bg-purple-900 rounded-2xl shadow-lg p-4 text-white">
-            {/* title */}
-            <div className="flex gap-4 text-2xl items-center">
-              {/* title icon */}
-              <span>
-                <GrAnnounce size={70} />
-              </span>
-              <h2 className="font-bold">Share Your Testimonies</h2>
+            {/* card 4 */}
+            <div className="flex flex-col justify-evenly w-[300px] h-[310px] bg-purple-900 rounded-2xl shadow-lg p-4 text-white">
+              {/* title */}
+              <div className="flex gap-4 text-2xl items-center">
+                {/* title icon */}
+                <span>
+                  <GrAnnounce size={70} />
+                </span>
+                <h2 className="font-bold">Share Your Testimonies</h2>
+              </div>
+              {/* content */}
+              <p className="font-bold text-zinc-400">
+                Share with us what the Lord has done for you
+              </p>
+              {/* call to action */}
+              <button className="border py-2 px-4 rounded-full w-fit ">
+                Share Your Testimonies
+              </button>
             </div>
-            {/* content */}
-            <p className="font-bold text-zinc-400">
-              Share with us what the Lord has done for you
-            </p>
-            {/* call to action */}
-            <button className="border py-2 px-4 rounded-full w-fit ">
-              Share Your Testimonies
-            </button>
-          </div>
 
-          {/* card 5 */}
-          <div
-            className="flex flex-col justify-evenly w-[300px] h-[310px] 
+            {/* card 5 */}
+            <div
+              className="flex flex-col justify-evenly w-[300px] h-[310px] 
           bg-sky-950 rounded-2xl shadow-lg p-4 text-white"
-          >
-            {/* title */}
-            <div className="flex gap-4 text-2xl items-center">
-              {/* title icon */}
-              <span>
-                <FaMoneyBillTransfer size={70} />
-              </span>
-              <h2 className="font-bold">Giving & Partnership</h2>
+            >
+              {/* title */}
+              <div className="flex gap-4 text-2xl items-center">
+                {/* title icon */}
+                <span>
+                  <FaMoneyBillTransfer size={70} />
+                </span>
+                <h2 className="font-bold">Giving & Partnership</h2>
+              </div>
+              {/* content */}
+              <p className="font-bold text-zinc-400 text-sm">
+                &quot;Give, and it will be given to you. A good measure, pressed
+                down, shaken together and running over, will be poured into your
+                lap. For with the measure you use, it will be measured to
+                you.&quot; Luke 6:38
+              </p>
+              {/* call to action */}
+              <button className="border py-2 px-4 rounded-full w-fit ">
+                Give Generously
+              </button>
             </div>
-            {/* content */}
-            <p className="font-bold text-zinc-400 text-sm">
-              &quot;Give, and it will be given to you. A good measure, pressed
-              down, shaken together and running over, will be poured into your
-              lap. For with the measure you use, it will be measured to
-              you.&quot; Luke 6:38
-            </p>
-            {/* call to action */}
-            <button className="border py-2 px-4 rounded-full w-fit ">
-              Give Generously
-            </button>
           </div>
         </div>
       </div>
@@ -221,7 +227,7 @@ export default function Home() {
         <h2 className="font-bold text-xl z-10">
           Our Live Service and Messages
         </h2>
-        <div className="w-full text-start pl-5 z-10">
+        <div className="w-full lg:w-[60%] text-start pl-5 lg:p-0 z-10">
           <p className="text-[#A11E1E] font-bold">Follow us on:</p>
           <SocialMedia />
         </div>
@@ -233,7 +239,7 @@ export default function Home() {
           </div>
         </div>
         {/* Explore media */}
-        <div className="text-[#A11E1E] font-bold pl-5 pr-10 flex gap-4 items-center z-10">
+        <div className="text-[#A11E1E] font-bold pl-5 pr-10 flex gap-4 items-center lg:justify-self-start z-10 lg:text-3xl">
           <span>
             <BsCollectionPlayFill size={30} />
           </span>
@@ -246,15 +252,15 @@ export default function Home() {
 
       {/* Instagram Posts */}
       <div className="w-full flex flex-col py-6 gap-3 items-center bg-[#D9D9D9]">
-        <div className="w-full px-5 text-start text-[#A11E1E] font-bold text-sm ">
+        <div className="w-full lg:w-[60%] px-5 text-start text-[#A11E1E] font-bold text-sm lg:text-xl ">
           <p>Our Recent Instagram Posts</p>
           <Link href={`/`} className="flex gap-2 items-center">
             <span>View More</span> <TbSquareRoundedArrowRightFilled />
           </Link>
         </div>
         {/* Post Container */}
-        <div className="w-[300px] min-h-[300px] bg-slate-100">
-          <div>
+        <div className="w-[300px] lg:w-[60%] min-h-[300px] bg-slate-100">
+          <div className="w-[100%]">
             <InstagramFeed />
           </div>
         </div>
@@ -267,7 +273,7 @@ export default function Home() {
           <p>Get Direction on Map</p>
         </div>
         {/* Google Map */}
-        <div className="min-h-[300px] w-[300px] bg-slate-700 ">
+        <div className="min-h-[300px] lg:w-[60%] bg-slate-700 ">
           <MapComponent />
         </div>
       </div>
--- src/components/weeklyService/WeeklyServices.tsx
+++ src/components/weeklyService/WeeklyServices.tsx
@@ -9,75 +9,83 @@ const WeeklyServices = () => {
         <h4 className={`text-sm text-slate-500`}>Worship With Us</h4>
         <h2 className="text-xl">Our Weekly Services</h2>
       </div>
-      {/* sunday service */}
-      <div className={`bg-slate-600 text-white w-[300px] h-[200px] z-30 relative`}>
-        <Image
-          src={`/guitar.jpg`}
-          alt=""
-          height={500}
-          width={400}
-          loading="lazy"
-          className="object-cover object-center h-full w-full"
-        />
+      <div className="flex flex-wrap w-full lg:w-[80%] gap-5 items-center justify-center">
+        {/* sunday service */}
         <div
-          className={`bg-slate-900 absolute flex flex-col justify-between top-0 left-0 w-[50%] h-full opacity-70 p-2`}
+          className={`bg-slate-600 text-white w-[300px] h-[200px] z-30 relative`}
         >
-          <h3>SUN.</h3>
-          <div>
-            <h4 className="bg-rose-800 px-2 text-center">Two Services</h4>
-            {/* early risers */}
+          <Image
+            src={`/guitar.jpg`}
+            alt=""
+            height={500}
+            width={400}
+            loading="lazy"
+            className="object-cover object-center h-full w-full"
+          />
+          <div
+            className={`bg-slate-900 absolute flex flex-col justify-between top-0 left-0 w-[50%] h-full opacity-70 p-2`}
+          >
+            <h3>SUN.</h3>
             <div>
-              <p>Early Risers</p>
-              <p className="text-slate-400 text-sm">8am-9:30am</p>
+              <h4 className="bg-rose-800 px-2 text-center">Two Services</h4>
+              {/* early risers */}
+              <div>
+                <p>Early Risers</p>
+                <p className="text-slate-400 text-sm">8am-9:30am</p>
+              </div>
+              {/* family service */}
+              <div>
+                <p>Family Service</p>
+                <p className="text-slate-400 text-sm">9:30am-11:30am</p>
+                <p></p>
+              </div>
             </div>
-            {/* family service */}
+          </div>
+        </div>
+        {/* Tuesday service */}
+        <div
+          className={`bg-slate-600 text-white w-[300px] h-[200px] z-30 relative`}
+        >
+          <Image
+            src={`/guitar.jpg`}
+            alt=""
+            height={500}
+            width={400}
+            loading="lazy"
+            className="object-cover object-center h-full w-full"
+          />
+          <div
+            className={`bg-slate-900 absolute flex flex-col justify-between top-0 left-0 w-[50%] h-full opacity-70 p-2`}
+          >
+            <h3>TUE.</h3>
             <div>
-              <p>Family Service</p>
-              <p className="text-slate-400 text-sm">9:30am-11:30am</p>
+              <p>Digging Deep</p>
+              <p className="text-slate-400 text-sm">6pm-7pm</p>
               <p></p>
             </div>
           </div>
         </div>
-      </div>
-      {/* Tuesday service */}
-      <div className={`bg-slate-600 text-white w-[300px] h-[200px] z-30 relative`}>
-        <Image
-          src={`/guitar.jpg`}
-          alt=""
-          height={500}
-          width={400}
-          loading="lazy"
-          className="object-cover object-center h-full w-full"
-        />
+        {/* Thursday service */}
         <div
-          className={`bg-slate-900 absolute flex flex-col justify-between top-0 left-0 w-[50%] h-full opacity-70 p-2`}
+          className={`bg-slate-600 text-white w-[300px] h-[200px] z-30 relative`}
         >
-          <h3>TUE.</h3>
-          <div>
-            <p>Digging Deep</p>
-            <p className="text-slate-400 text-sm">6pm-7pm</p>
-            <p></p>
-          </div>
-        </div>
-      </div>
-      {/* Thursday service */}
-      <div className={`bg-slate-600 text-white w-[300px] h-[200px] z-30 relative`}>
-        <Image
-          src={`/guitar.jpg`}
-          alt=""
-          height={500}
-          width={400}
-          loading="lazy"
-          className="object-cover object-center h-full w-full"
-        />
-        <div
-          className={`bg-slate-900 absolute flex flex-col justify-between top-0 left-0 w-[50%] h-full opacity-70 p-2`}
-        >
-          <h3>THUR.</h3>
-          <div>
-            <p>Faith Clinic</p>
-            <p className="text-slate-400 text-sm">6pm-7pm</p>
-            <p></p>
+          <Image
+            src={`/guitar.jpg`}
+            alt=""
+            height={500}
+            width={400}
+            loading="lazy"
+            className="object-cover object-center h-full w-full"
+          />
+          <div
+            className={`bg-slate-900 absolute flex flex-col justify-between top-0 left-0 w-[50%] h-full opacity-70 p-2`}
+          >
+            <h3>THUR.</h3>
+            <div>
+              <p>Faith Clinic</p>
+              <p className="text-slate-400 text-sm">6pm-7pm</p>
+              <p></p>
+            </div>
           </div>
         </div>
       </div>
