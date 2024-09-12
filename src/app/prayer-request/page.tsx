6035192008455a6a238b2402cbb3b801d5625be7
lightpc <afoageektech@gmail.com>
2024-09-12 14:19:07 +0100

    "Updated Prayer Request page and form with new layout, styles, and fields"

--- src/app/prayer-request/page.tsx
+++ src/app/prayer-request/page.tsx
@@ -2,18 +2,31 @@ import Header from "@/components/UILayouts/Header";
 import React from "react";
 import PrayerRequestForm from "@/components/UILayouts/PrayerRequestForm";
 import { PrayerRequestPageIntro } from "@/components/data/Data";
+import prayerImage from "../../../public/headers/prayer-request.png"
 
 const Page = () => {
   return (
-    <div>
-      <Header imageUrl="" title={PrayerRequestPageIntro.headerTitle} />
+    <div className="bg-[#D9D9D9] flex flex-col w-full items-center">
+      <Header
+        imageUrl={prayerImage.src}
+        title={PrayerRequestPageIntro.headerTitle}
+      />
       {/* content body */}
-      <div>
-        <h3>{PrayerRequestPageIntro.title}</h3>
-        <p>{PrayerRequestPageIntro.description}</p>
+      <div className="p-5">
+        <h3 className="font-bold text-xl text-center">
+          {PrayerRequestPageIntro.title}
+        </h3>
+        <p className="mb-5 text-center text-blue-950">
+          {PrayerRequestPageIntro.description}
+        </p>
         <p>{PrayerRequestPageIntro.body}</p>
+        <p className="py-10 text-blue-950">
+          Kinldy fill the form below to submit your prayer request so that we
+          can pray with you
+        </p>
       </div>
       {/* Prayer request form */}
+
       <div>
         <PrayerRequestForm />
       </div>
--- src/components/UILayouts/PrayerRequestForm.tsx
+++ src/components/UILayouts/PrayerRequestForm.tsx
@@ -1,33 +1,52 @@
 import React from "react";
 
-const PrayerRequestForm = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+const NewConvertForm = () => {
+  const handleSubmit = () => {};
   return (
-    <div>
-      <h1>Prayer Request Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
+    <div className="flex flex-col items-center gap-3 ">
+      <h1 className="font-bold text-lg text-blue-950">Prayer Request Form</h1>
+      <div>
+        <div className="flex flex-col mb-2">
           <label htmlFor="fullName">Full Name</label>
-          <input id="fullName" type="text" placeholder="John Doe" />
+          <input
+            id="fullName"
+            type="text"
+            placeholder="John Doe"
+            className="bg-[#FFFFFF] p-2 rounded-md"
+          />
         </div>
-        <div>
+        <div className="flex flex-col mb-2">
           <label htmlFor="phone">Phone Number</label>
-          <input id="phone" type="number" placeholder="08123456789" />
+          <input
+            id="phone"
+            type="number"
+            placeholder="08123456789"
+            className="bg-[#FFFFFF] p-2 rounded-md"
+          />
         </div>
-        <div>
+        <div className="flex flex-col mb-2">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" placeholder="example@gmail.com" />
+          <input
+            id="email"
+            type="email"
+            placeholder="example@gmail.com"
+            className="bg-[#FFFFFF] p-2 rounded-md"
+          />
         </div>
-        <div>
-          <label htmlFor="address">Your Prayer Request</label>
-          <textarea id="address" placeholder="Let's pray with you" />
+        <div className="flex flex-col mb-2">
+          <label htmlFor="prayer-request">Prayer Request</label>
+          <textarea
+            id="prayer-request"
+            placeholder="1, Simpson str. Lakowe phase 2"
+            className="bg-[#FFFFFF] p-2 rounded-md"
+          />
         </div>
-        <input type="submit" value="Pray With Me" />
-      </form>
+        <button className="bg-blue-950 hover:bg-blue-800 p-2 text-white rounded-md w-full my-2">
+          Pray With Me
+        </button>
+      </div>
     </div>
   );
 };
 
-export default PrayerRequestForm;
+export default NewConvertForm;
