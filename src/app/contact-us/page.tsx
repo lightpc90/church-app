5936c7a42417f0004eb2baa81901e4e89199f418
lightpc <afoageektech@gmail.com>
2024-09-17 20:18:47 +0100

    "Updated Contact Us page with new layout, added header image, map component, and enquiry form"

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import Header from '@/components/UILayouts/Header'
-
+import React from "react";
+import Header from "@/components/UILayouts/Header";
+import headerImage from "../../../public/headers/Ch6.jpg";
+import MapComponent from "@/components/map/GoogleMap";
+import EnquiryForm from "@/components/UILayouts/EnquiryForm";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="bg-[#D9D9D9]">
+      <Header title="Contact Us" imageUrl={headerImage.src} />
+      {/* content */}
+      <div className="px-5 my-10">
+        <div>
+          <div>
+            <span className="text-rose-800 font-bold">RCCG Beautiful Gate</span> is home for all. We are glad to
+            have you
+          </div>
+        </div>
+        {/* map */}
+        <div className="text-center my-3">
+          <p className="text-slate-400">Locate us on map</p>
+          <h3 className="font-bold">Get Direction</h3>
+          <MapComponent />
+        </div>
+        {/* enquiry form */}
+        <div>
+          <div>
+            <h4>For Enquiry</h4>
+            <h3>Kindly fill the form below</h3>
+          </div>
+          <EnquiryForm />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
---
+++ src/components/UILayouts/EnquiryForm.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const EnquiryForm = () => {
+  const handleSubmit = () => {};
+  return (
+    <div className="flex flex-col items-center gap-3 ">
+      <h1 className="font-bold text-lg text-blue-950">Enquiry Form</h1>
+      <div>
+        <div className="flex flex-col mb-2">
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            id="fullName"
+            type="text"
+            placeholder="John Doe"
+            className="bg-[#FFFFFF] p-2 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col mb-2">
+          <label htmlFor="phone">Phone Number</label>
+          <input
+            id="phone"
+            type="number"
+            placeholder="08123456789"
+            className="bg-[#FFFFFF] p-2 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col mb-2">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="example@gmail.com"
+            className="bg-[#FFFFFF] p-2 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col mb-2">
+          <label htmlFor="address">Your Enquiry</label>
+          <textarea
+            id="enquiry"
+            placeholder="1, Simpson str. Lakowe phase 2"
+            className="bg-[#FFFFFF] p-2 rounded-md"
+          />
+        </div>
+        <button className="bg-blue-950 hover:bg-blue-800 p-2 text-white rounded-md w-full my-2">
+          Send Enquiry
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EnquiryForm;
