535078c50f2fddb6749d3c6fd088f4940cf08307
lightpc <afoageektech@gmail.com>
2024-09-18 08:25:31 +0100

    "Updated navigation links in workers pages to use Next.js Link component"

--- src/app/workers/create-account/page.tsx
+++ src/app/workers/create-account/page.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import TermsAndPrivacy from '@/components/UILayouts/TermsAndPrivacy'
 import { IoMdArrowRoundBack } from "react-icons/io";
+import Link from 'next/link';
 
 const Page = () => {
   return (
     <div className="flex flex-col items-center justify-between w-full h-screen-dynamic px-5 pb-10 pt-[150px]">
       {/* CTA */}
       <div className="flex flex-col items-center w-full gap-6">
-        <div className="relative px-[58px] text-center flex">
+        <Link href={`/workers`} className="relative px-[58px] text-center flex">
           <div className='absolute top-0 left-0'><IoMdArrowRoundBack size={25}/></div>
           <p className="text-[#EE7575] font-bold">Create account</p>
-        </div>
+        </Link>
         <div className="w-full text-center">
           <h4 className='font-bold text-blue-950'>Glad to have you!</h4>
           <p>Let&apos;s work</p>
--- src/app/workers/page.tsx
+++ src/app/workers/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import bg from "../../../public/headers/workers.png";
 import Image from "next/image";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import { AiFillHome } from "react-icons/ai";
 
 const Page = () => {
   return (
@@ -13,7 +14,7 @@ const Page = () => {
         className="h-full w-full object-cover object-center"
       />
       {/* gradient overlay */}
-      <div className="px-5 py-10 absolute top-0 left-0 w-full h-full bg-gradient-to-t flex flex-col items-center justify-between from-slate-950 via-slate-700 opacity-90 ">
+      <div className="px-5 py-[50px] absolute top-0 left-0 w-full h-full bg-gradient-to-t flex flex-col items-center justify-between from-slate-950 via-slate-700 opacity-90 ">
         <Link
           href={`/`}
           className="text-white flex gap-2 items-center text-left w-full"
@@ -21,17 +22,17 @@ const Page = () => {
           <span>
             <IoMdArrowRoundBack size={25} />
           </span>
-          <span>Back</span>
+          <span><AiFillHome size={25}/></span>
         </Link>
         <div className="flex flex-col items-center gap-1 z-30">
           <h3 className="text-white">RCCG Beautiful Gate, Lakowe</h3>
           <p className="text-[#EE7575]">Workers&apos; space</p>
-          <button className="py-2 px-10 bg-white text-slate-950 font-bold shadow-lg rounded-md z-30">
+          <Link href={`/workers/create-account`} className="block py-2 px-10 bg-white text-slate-950 font-bold shadow-lg rounded-md z-30">
             Register With Gmail
-          </button>
+          </Link>
           <div className="flex gap-2 items-center text-white">
             <p>Already have an account?</p>
-            <Link href={``}>Login</Link>
+            <Link href={`/workers/login`}>Login</Link>
           </div>
         </div>
       </div>
