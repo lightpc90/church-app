35bc3e5609a3a8daaafba6c798ef1df991103e11
lightpc <afoageektech@gmail.com>
2024-09-23 18:22:04 +0100

    "Updated imports and layout of Online Giving page, added centering and width styles to content sections."

--- src/app/online-giving/page.tsx
+++ src/app/online-giving/page.tsx
@@ -1,51 +1,54 @@
-import Header from '@/components/UILayouts/Header'
-import React from 'react'
-import AccountLayout from '@/components/UILayouts/AccountLayout'
-import { AccountData } from '@/components/data/Data'
-import givingImage from "../../../public/headers/offering.png"
-import bankLogo from "../../../public/headers/zenith-logo.jpg"
+import Header from "@/components/UILayouts/Header";
+import React from "react";
+import AccountLayout from "@/components/UILayouts/AccountLayout";
+import { AccountData } from "@/components/data/Data";
+import givingImage from "../../../public/headers/offering.png";
+import bankLogo from "../../../public/headers/zenith-logo.jpg";
 
 const Page = () => {
   return (
-    <div className="bg-[#D9D9D9] flex flex-col w-full gap-5">
+    <div className="bg-[#D9D9D9] flex flex-col w-full gap-5 items-center">
       <Header title="Online Giving" imageUrl={givingImage.src} />
-      {/* tithe and offering */}
-      <div className="flex flex-col gap-3 items-center">
-        <h3 className="font-bold text-xl">Tithe and Offering Account</h3>
-        {AccountData.titheAndOffering.map((account, i) => (
-          <AccountLayout key={i} account={account} bankLogo={bankLogo.src} />
-        ))}
-      </div>
-      {/* welfare */}
-      <div className="flex flex-col gap-3 items-center">
-        <h3 className="font-bold text-xl">Welfare Account</h3>
-        {AccountData.welfare.map((account, i) => (
-          <AccountLayout key={i} account={account} bankLogo={bankLogo.src} />
-        ))}
-      </div>
-      {/* Building account */}
-      <div className="flex flex-col gap-3 items-center">
-        <h3 className="font-bold text-xl">Building Account</h3>
-        {AccountData.building.map((account, i) => (
-          <AccountLayout key={i} account={account} bankLogo={bankLogo.src} />
-        ))}
-      </div>
-      {/* Mission account */}
-      <div className="flex flex-col gap-3 items-center">
-        <h3 className="font-bold text-xl">Mission Account</h3>
-        {AccountData.mission.map((account, i) => (
-          <AccountLayout key={i} account={account} bankLogo={bankLogo.src} />
-        ))}
-      </div>
-      {/* Youth account */}
-      <div className="flex flex-col gap-3 items-center">
-        <h3 className="font-bold text-xl">Youth Account</h3>
-        {AccountData.youth.map((account, i) => (
-          <AccountLayout key={i} account={account} bankLogo={bankLogo.src} />
-        ))}
+      {/* content */}
+      <div className="flex flex-col items-center w-full lg:w-[60%]">
+        {/* tithe and offering */}
+        <div className="flex flex-col gap-3 items-center w-full">
+          <h3 className="font-bold text-xl">Tithe and Offering Account</h3>
+          {AccountData.titheAndOffering.map((account, i) => (
+            <AccountLayout key={i} account={account} bankLogo={bankLogo.src} />
+          ))}
+        </div>
+        {/* welfare */}
+        <div className="flex flex-col gap-3 items-center w-full">
+          <h3 className="font-bold text-xl">Welfare Account</h3>
+          {AccountData.welfare.map((account, i) => (
+            <AccountLayout key={i} account={account} bankLogo={bankLogo.src} />
+          ))}
+        </div>
+        {/* Building account */}
+        <div className="flex flex-col gap-3 items-center w-full">
+          <h3 className="font-bold text-xl">Building Account</h3>
+          {AccountData.building.map((account, i) => (
+            <AccountLayout key={i} account={account} bankLogo={bankLogo.src} />
+          ))}
+        </div>
+        {/* Mission account */}
+        <div className="flex flex-col gap-3 items-center w-full">
+          <h3 className="font-bold text-xl">Mission Account</h3>
+          {AccountData.mission.map((account, i) => (
+            <AccountLayout key={i} account={account} bankLogo={bankLogo.src} />
+          ))}
+        </div>
+        {/* Youth account */}
+        <div className="flex flex-col gap-3 items-center w-full">
+          <h3 className="font-bold text-xl">Youth Account</h3>
+          {AccountData.youth.map((account, i) => (
+            <AccountLayout key={i} account={account} bankLogo={bankLogo.src} />
+          ))}
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Page
+export default Page;
