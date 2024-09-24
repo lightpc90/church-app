89b907056a7282ba9f24c0e60525bfbd829ee8e9
lightpc <afoageektech@gmail.com>
2024-09-24 08:02:58 +0100

    "Updated prayer request and testimony pages with layout and styling changes"

--- src/app/prayer-request/page.tsx
+++ src/app/prayer-request/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/UILayouts/Header";
 import React from "react";
 import PrayerRequestForm from "@/components/UILayouts/PrayerRequestForm";
 import { PrayerRequestPageIntro } from "@/components/data/Data";
-import prayerImage from "../../../public/headers/prayer-request.png"
+import prayerImage from "../../../public/headers/prayer-request.png";
 
 const Page = () => {
   return (
@@ -12,23 +12,25 @@ const Page = () => {
         title={PrayerRequestPageIntro.headerTitle}
       />
       {/* content body */}
-      <div className="p-5">
-        <h3 className="font-bold text-xl text-center">
-          {PrayerRequestPageIntro.title}
-        </h3>
-        <p className="mb-5 text-center text-blue-950">
-          {PrayerRequestPageIntro.description}
-        </p>
-        <p>{PrayerRequestPageIntro.body}</p>
-        <p className="py-10 text-blue-950">
-          Kinldy fill the form below to submit your prayer request so that we
-          can pray with you
-        </p>
-      </div>
-      {/* Prayer request form */}
+      <div className="lg:w-[70%] flex flex-col items-center">
+        <div className="p-5 lg:w-[50%] flex flex-col items-center">
+          <h3 className="font-bold text-xl lg:text-3xl text-center">
+            {PrayerRequestPageIntro.title}
+          </h3>
+          <p className="lg:text-xl mb-5 text-center text-blue-950">
+            {PrayerRequestPageIntro.description}
+          </p>
+          <p className="lg:text-lg">{PrayerRequestPageIntro.body}</p>
+          <p className="py-10 text-blue-950 lg:text-lg lg:font-bold lg:w-[70%] text-center">
+            Kinldy fill the form below to submit your prayer request so that we
+            can pray with you
+          </p>
+        </div>
+        {/* Prayer request form */}
 
-      <div>
-        <PrayerRequestForm />
+        <div className="w-full flex justify-center">
+          <PrayerRequestForm />
+        </div>
       </div>
     </div>
   );
--- src/app/testimony/page.tsx
+++ src/app/testimony/page.tsx
@@ -9,12 +9,12 @@ const Page = () => {
     <div className='bg-[#D9D9D9] flex flex-col items-center gap-3'>
         <Header imageUrl={TestimonyImage.src} title='Share Your Testimony With Us'/>
         {/* content */}
-        <div className='p-5'>
-            <h3 className='font-bold text-xl'>The Lord is Good and His mercies Endure forever</h3>
-            <p className='text-blue-950'>Kindly fill the form below to share with us what God has done for you</p>
+        <div className='p-5 text-center space-y-2'>
+            <h3 className='font-bold text-xl lg:text-3xl'>The Lord is Good and His mercies Endure forever</h3>
+            <p className='text-blue-950 lg:font-bold lg:text-xl'>Kindly fill the form below to share with us what God has done for you</p>
         </div>
         {/* Testimony Form */}
-        <div>
+        <div className='flex w-full justify-center'>
             <TestimonyForm/>
         </div>
     </div>
