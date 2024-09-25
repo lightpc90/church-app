8278342fe298424329d551346f19015346470beb
lightpc <afoageektech@gmail.com>
2024-09-25 18:52:02 +0100

    "Updated HomeHeaderTitle component: removed overflow-hidden class, changed transition delay values"

--- src/components/home_header_title/HomeHeaderTitle.tsx
+++ src/components/home_header_title/HomeHeaderTitle.tsx
@@ -5,11 +5,11 @@ import { easeOut, motion } from 'framer-motion';
 
 const HomeHeaderTitle = () => {
   return (
-    <div className="flex flex-col text-white gap-1 lg:gap-4 font-bold w-10/12 lg:w-5/12 mt-[50px] overflow-hidden ">
+    <div className="flex flex-col text-white gap-1 lg:gap-4 font-bold w-10/12 lg:w-5/12 mt-[50px] ">
       <motion.p
         initial={{ opacity: 0, x: -100, scale: 0.8 }}
         animate={{ opacity: 1, x: 0, scale: 1 }}
-        transition={{ duration: 0.5, delay: 0.3, ease: easeOut }}
+        transition={{ duration: 0.5, delay: 0.2, ease: easeOut }}
         className="text-2xl lg:text-4xl overflow-hidden"
       >
         Experience God&apos;s Love through worship and fellowship at our Church.
@@ -17,7 +17,7 @@ const HomeHeaderTitle = () => {
       <motion.p
         initial={{ opacity: 0, x: -50, scale: 0.8 }}
         animate={{ opacity: 1, x: 0, scale: 1 }}
-        transition={{ duration: 0.5, delay: 0.1, ease: easeOut }}
+        transition={{ duration: 0.5, delay: 0.5, ease: easeOut }}
         className="lg:text-2xl text-zinc-400"
       >
         Experience a welcoming community and uplifting sermons at our church.
