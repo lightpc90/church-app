0fea95a779fa1c6892d8ec4eb886135c9605b916
lightpc <afoageektech@gmail.com>
2024-10-03 02:16:24 +0100

    "Added 'use client' directive to Menu.tsx, imported useState and easeOut from react and framer-motion respectively in Navbar.tsx, and wrapped mobile navigation links with AnimatePresence and motion.div from framer-motion."

--- src/components/navbar/Menu.tsx
+++ src/components/navbar/Menu.tsx
@@ -1,5 +1,7 @@
+'use client'
+
 import Link from 'next/link'
-import React from 'react'
+import {useState} from 'react'
 
 const Menu = () => {
   return (
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,12 @@
-'use client'
+"use client";
 
-import {useState} from "react";
+import { useState } from "react";
 import Logo from "./Logo";
 import Links from "./Links";
 import Link from "next/link";
 import MobileNavbar from "./MobileNavbar";
+
+import { easeOut, motion, AnimatePresence } from "framer-motion";
 
 import { AiFillHome } from "react-icons/ai";
 import { AiFillSafetyCertificate } from "react-icons/ai";
@@ -19,7 +21,6 @@ import { BsFillCalendarEventFill } from "react-icons/bs";
 import { RiCustomerServiceFill } from "react-icons/ri";
 import { BsFillBriefcaseFill } from "react-icons/bs";
 import SocialMedia from "../social-media/SocialMedia";
-
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -41,7 +42,7 @@ const Navbar = () => {
           >
             login
           </Link>
-          
+
           {/* profile picture wrapper */}
           <div className="lg:hidden">
             <MobileNavbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
@@ -49,125 +50,134 @@ const Navbar = () => {
         </div>
       </div>
       {/* mobile links */}
-      {menuOpen && (
-        <div className="bg-zinc-900  pt-[90px] fixed top-0 left-0 w-full h-full z-40">
-          {/* links */}
-          <div className="flex flex-col w-full gap-3 text-white h-[80%] overflow-auto">
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex gap-2 items-center"
-              href={`/`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <AiFillHome />
-              Home
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex items-center gap-2 "
-              href={`/how-can-i-be-saved`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <AiFillSafetyCertificate />
-              How Can I Be Saved?
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex gap-2 items-center "
-              href={`/online-church`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <SiWikimediafoundation />
-              Online Church
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex gap-2 items-center "
-              href={`/online-giving`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <GiGiftOfKnowledge />
-              Online Giving
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex gap-2 items-center "
-              href={`/prayer-request`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <GiPrayer />
-              Prayer Request
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex gap-2 items-center "
-              href={`/testimony`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <RiUserSharedFill />
-              Share Testimony
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex gap-2 items-center "
-              href={`/workers`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <MdWorkspacePremium />
-              Workers
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex gap-2 items-center "
-              href={`/groups-and-ministries`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <HiMiniUserGroup />
-              Our Natural Groups and Ministries
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex gap-2 items-center "
-              href={`/house-fellowship-centers`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <BiSolidBuildingHouse />
-              House Fellowship Centers
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex gap-2 items-center "
-              href={`/events`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <BsFillCalendarEventFill />
-              Events
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex gap-2 items-center "
-              href={`/contact-us`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <RiCustomerServiceFill />
-              Contact
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-            <Link
-              className="font-bold pl-5 flex gap-2 items-center "
-              href={`/about-us`}
-              onClick={() => setMenuOpen(false)}
-            >
-              <BsFillBriefcaseFill />
-              About Us
-            </Link>
-            <div className="w-full h-[1px] bg-gray-400"></div>
-          </div>
-          <div className="text-white text-center w-full pl-5 pt-5">
-            <SocialMedia />
-          </div>
-        </div>
-      )}
+      <AnimatePresence>
+        {menuOpen && (
+          <motion.div
+            key="mobbileNav"
+            initial={{ x: "-100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "-100%" }}
+            transition={{ duration: 0.7, ease: easeOut }}
+            className="bg-zinc-900  pt-[90px] fixed top-0 left-0 w-full h-full z-40"
+          >
+            {/* links */}
+            <div className="flex flex-col w-full gap-3 text-white h-[80%] overflow-auto">
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex gap-2 items-center"
+                href={`/`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <AiFillHome />
+                Home
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex items-center gap-2 "
+                href={`/how-can-i-be-saved`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <AiFillSafetyCertificate />
+                How Can I Be Saved?
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex gap-2 items-center "
+                href={`/online-church`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <SiWikimediafoundation />
+                Online Church
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex gap-2 items-center "
+                href={`/online-giving`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <GiGiftOfKnowledge />
+                Online Giving
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex gap-2 items-center "
+                href={`/prayer-request`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <GiPrayer />
+                Prayer Request
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex gap-2 items-center "
+                href={`/testimony`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <RiUserSharedFill />
+                Share Testimony
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex gap-2 items-center "
+                href={`/workers`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <MdWorkspacePremium />
+                Workers
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex gap-2 items-center "
+                href={`/groups-and-ministries`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <HiMiniUserGroup />
+                Our Natural Groups and Ministries
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex gap-2 items-center "
+                href={`/house-fellowship-centers`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <BiSolidBuildingHouse />
+                House Fellowship Centers
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex gap-2 items-center "
+                href={`/events`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <BsFillCalendarEventFill />
+                Events
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex gap-2 items-center "
+                href={`/contact-us`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <RiCustomerServiceFill />
+                Contact
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+              <Link
+                className="font-bold pl-5 flex gap-2 items-center "
+                href={`/about-us`}
+                onClick={() => setMenuOpen(false)}
+              >
+                <BsFillBriefcaseFill />
+                About Us
+              </Link>
+              <div className="w-full h-[1px] bg-gray-400"></div>
+            </div>
+            <div className="text-white text-center w-full pl-5 pt-5">
+              <SocialMedia />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
