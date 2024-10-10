81760e6f69b84717d0d7d36ef63848aaec36605c
lightpc <afoageektech@gmail.com>
2024-10-10 16:09:46 +0100

    "Update next.config.mjs: removed remotePatterns and replaced with domains"

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "instagram.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "scontent-los2-1.cdninstagram.com", // another external domain
-        pathname: "/**", // specific path if needed
-      },
-    ],
+    domains: ["scontent-iad3-1.cdninstagram.com"],
   },
 };
 
