79104686e66b56deeba411cf18ef4e08e6f87f85
lightpc <afoageektech@gmail.com>
2024-10-23 11:51:41 +0100

    "Added form fields and layout to GroupModal component"

--- src/components/UILayouts/GroupModal.tsx
+++ src/components/UILayouts/GroupModal.tsx
@@ -14,42 +14,158 @@ type GroupLayoutPropsType = {
   };
 };
 
+const formInitial = { fullname: "", phone: "", email: "", gender: "" };
+
 const GroupModal: FC<GroupLayoutPropsType> = ({ group }) => {
+  let genderInput;
+  if (group.name === "Good Women" || group.name === "Excellent Men") {
+    genderInput = false;
+  } else genderInput = true;
+
+  const childrenReg = group.name === "Children Ministry";
   const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState(formInitial);
   return (
     <div>
       {/* modal component */}
       <AnimatePresence>
-
-      {showModal && (
-        <motion.div 
-        key="groupmodal"
-        initial={{y: "100%"}}
-        animate={{y: 0}}
-        exit={{y: "100%"}}
-        transition={{duration: 0.5, ease: easeOut}}
-        
-        className="fixed inset-0 w-full h-[100vh] z-[100] flex justify-center">
-          <div
-            className={`w-full absolute bottom-0 bg-white ring-4 shadow-2xl h-[85vh] p-3`}
-            style={{
-              borderTopLeftRadius: "40px",
-              borderTopRightRadius: "40px",
-            }}
+        {showModal && (
+          <motion.div
+            key="groupmodal"
+            initial={{ y: "100%" }}
+            animate={{ y: 0 }}
+            exit={{ y: "100%" }}
+            transition={{ duration: 0.5, ease: easeOut }}
+            className="fixed inset-0 w-full h-[100vh] z-[100] flex justify-center"
           >
-            <div className="absolute right-4">
+            <div
+              className={`w-full absolute bottom-0 bg-white ring-4 shadow-2xl h-[85vh] p-3`}
+              style={{
+                borderTopLeftRadius: "40px",
+                borderTopRightRadius: "40px",
+              }}
+            >
+              <div className="absolute right-4">
+                <button onClick={() => setShowModal(false)}>
+                  <LuPanelBottomClose size={25} className=" text-blue-500" />
+                </button>
+              </div>
+              <h2 className="font-semibold mb-5">
+                Groups and Ministries Database Form
+              </h2>
+              <h2 className="font-bold text-xl mb-5">
+                {group.name} Registration Form
+              </h2>
+              <div className="mt-3 flex flex-col justify-center h-[86%] overflow-auto">
+                <p className="text-blue-900 font-semibold">Parent/Guadian Details Section</p>
+                {/* form */}
+                <div className="space-y-2">
+                  {/* full name */}
+                  <div className="flex flex-col gap-1">
+                    <label htmlFor="fullname">Full Name</label>
+                    <input
+                      type="text"
+                      value={formData.fullname}
+                      placeholder="John David"
+                      required
+                      className="p-2 border-l-4 border-blue-800"
+                    />
+                  </div>
+                  {/* full name */}
+                  <div className="flex flex-col gap-1">
+                    <label htmlFor="phone">Phone Number</label>
+                    <input
+                      type="number"
+                      value={formData.phone}
+                      placeholder="1234567890"
+                      required
+                      className="p-2 border-l-4 border-blue-800"
+                    />
+                  </div>
+                  {/* email */}
+                  <div className="flex flex-col gap-1">
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="email"
+                      value={formData.email}
+                      placeholder="example@gmail.com"
+                      className="p-2 border-l-4 border-blue-800"
+                    />
+                  </div>
+                  {/* gender */}
+                  {genderInput && (
+                    <div className="flex flex-col gap-1">
+                      <label htmlFor="gender">Gender</label>
+                      <div className="flex gap-5">
+                        <label className="flex items-center font-semibold gap-2">
+                          Male
+                          <input
+                            type="radio"
+                            value="male"
+                            name="gender"
+                            required
+                          />
+                        </label>
+                        <label className="flex items-center font-semibold gap-2">
+                          Female
+                          <input type="radio" value="female" name="gender" />
+                        </label>
+                      </div>
+                    </div>
+                  )}
+                  <hr style={{marginTop: "30px", marginBottom: "30px"}}/>
+                  {/* children details section */}
 
-            <button onClick={() => setShowModal(false)}>
-              <LuPanelBottomClose size={25} className=" text-blue-500" />
-            </button>
+                  {childrenReg && (
+                    <div className="flex flex-col gap-3">
+                      <p className=" text-blue-900 font-semibold">{`Child's Details Section`}</p>
+                      {/* full name */}
+                      <div className="flex flex-col gap-1">
+                        <label htmlFor="fullname">{`Full Name`}</label>
+                        <input
+                          type="text"
+                          value={formData.fullname}
+                          placeholder="John David"
+                          required
+                          className="p-2 border-l-4 border-blue-800"
+                        />
+                      </div>
+                      {/* gender */}
+                      <div className="flex flex-col gap-1">
+                        <label htmlFor="gender">Gender</label>
+                        <div className="flex gap-5">
+                          <label className="flex items-center font-semibold gap-2">
+                            Male
+                            <input
+                              type="radio"
+                              value="male"
+                              name="gender"
+                              required
+                            />
+                          </label>
+                          <label className="flex items-center font-semibold gap-2">
+                            Female
+                            <input type="radio" value="female" name="gender" />
+                          </label>
+                        </div>
+                      </div>
+                      {/* DOB */}
+                      <div className="flex gap-4 items-center flex-wrap">
+                        <label htmlFor="dob">Date of Birth</label>
+                        <input name="dob" type="date" />
+                      </div>
+                    </div>
+                  )}
+
+                  {/* submit */}
+                  <button className="mt-10 inline-block bg-blue-800 p-2 text-white rounded-md">
+                    Submit
+                  </button>
+                </div>
+              </div>
             </div>
-            {/* form */}
-            <div className="mt-5 flex flex-col items-center justify-center h-[86%] overflow-auto">
-            <h2 className="font-bold text-xl">{group.name} Registration Form</h2>
-            </div>
-          </div>
-        </motion.div>
-      )}
+          </motion.div>
+        )}
       </AnimatePresence>
 
       <button
