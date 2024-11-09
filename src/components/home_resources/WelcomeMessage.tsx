a9fe33e1ebd8ebe61be93fbaf13d60bc4cc55fc1
lightpc <afoageektech@gmail.com>
2024-11-09 01:12:21 +0100

    "Updated WelcomeMessage component: adjusted CSS classes and properties, and reformatted code for better readability."

--- src/components/home_resources/WelcomeMessage.tsx
+++ src/components/home_resources/WelcomeMessage.tsx
@@ -9,14 +9,16 @@ import { easeIn, easeOut, motion } from "framer-motion";
 
 const WelcomeMessage = () => {
   return (
-    <div id="intro" className="flex flex-wrap gap-4 lg:gap-10 w-[100vw] overflow-hidden p-[20px] items-center justify-center my-10 ">
-     
-      <div className="w-full lg:w-[30%]">
+    <div
+      id="intro"
+      className="flex flex-wrap gap-4 lg:gap-10 w-[100vw] md:w-[70%] overflow-hidden p-[20px] items-center justify-center my-10 "
+    >
+      <div className="w-full lg:w-[30%] ">
         <motion.div
-          initial={{ opacity: 0, scale: 0.8}}
+          initial={{ opacity: 0, scale: 0.8 }}
           whileInView={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5, ease: easeOut }}
-          viewport={{once:true}}
+          viewport={{ once: true }}
           className="text-center"
         >
           <h1 className="text-2xl text-center font-bold lg:text-3xl">
@@ -30,7 +32,7 @@ const WelcomeMessage = () => {
           initial={{ opacity: 0, scale: 0.8 }}
           transition={{ duration: 0.5, ease: easeOut }}
           whileInView={{ opacity: 1, scale: 1 }}
-          viewport={{once: true}}
+          viewport={{ once: true }}
           className="text-justify lg:text-2xl"
         >
           {ChurchWelcomeParagraph.paragraph}
@@ -41,8 +43,8 @@ const WelcomeMessage = () => {
         initial={{ opacity: 0, scale: 0.8 }}
         transition={{ duration: 0.5, ease: easeOut }}
         whileInView={{ opacity: 1, scale: 1 }}
-        viewport={{once: true}}
-        className="w-full lg:w-[40%] rounded-md overflow-hidden shadow-md h-[400px] lg:h-[600px] bg-slate-300"
+        viewport={{ once: true }}
+        className="w-full lg:w-[40%]  rounded-md overflow-hidden shadow-md h-[400px] lg:h-[600px] bg-slate-300"
       >
         {/* Pastor Picture coming here */}
         <Image
