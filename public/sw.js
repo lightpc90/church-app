ab50532820f8e362a33585e9a04638d7bbb872c7
lightpc <afoageektech@gmail.com>
2024-11-30 08:56:56 +0100

    "Removed Apache license header and reformatted code in sw.js and updated sw.js.map"

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,4 @@
-/**
- * Copyright 2018 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 // If the loader is already loaded, just stop.
 if (!self.define) {
@@ -74,7 +63,8 @@ define(['./workbox-631a4576'], (function (workbox) { 'use strict';
   workbox.clientsClaim();
   workbox.registerRoute("/", new workbox.NetworkFirst({
     "cacheName": "start-url",
-    plugins: [{
+    plugins: [
+      {
       cacheWillUpdate: async ({
         response: e
       }) => e && "opaqueredirect" === e.type ? new Response(e.body, {
@@ -82,7 +72,8 @@ define(['./workbox-631a4576'], (function (workbox) { 'use strict';
         statusText: "OK",
         headers: e.headers
       }) : e
-    }]
+    }
+  ]
   }), 'GET');
   workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
     "cacheName": "dev",
