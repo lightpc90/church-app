60ac7535453c50138d0e5210d91f662b62027c90
lightpc <afoageektech@gmail.com>
2024-12-04 13:00:06 +0100

    "Updated metadata in layout.tsx: added siteURL, openGraph, and appLinks properties, and modified robots settings"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ const APP_NAME = "RCCG Beautiful Gate Progressive Web App"
 const APP_DEFAULT_TITLE = "Beautiful Gate Parish"
 const APP_TITLE_TEMPLATE = "%s - Progressive Web App"
 const APP_DESCRIPTION = "A Progressive Web App for RCCG Beautiful Gate"
+const siteURL = "https://beautiful-gate.vercel.app/"
 
 export const metadata: Metadata = {
   applicationName: APP_NAME,
@@ -25,13 +26,15 @@ export const metadata: Metadata = {
   description: APP_DESCRIPTION,
 
   openGraph: {
-    type: "website",
-    siteName: APP_NAME,
     title: {
       default: APP_DEFAULT_TITLE,
       template: APP_TITLE_TEMPLATE,
     },
     description: APP_DESCRIPTION,
+    url: siteURL,
+    siteName: APP_NAME,
+    images: ["/icons/icon192x192.png"],
+    type: "website",
   },
   twitter: {
     card: "summary",
@@ -42,15 +45,31 @@ export const metadata: Metadata = {
     description: APP_DESCRIPTION,
     creator: "@_lightpc_",
     images: ["/icons/icon192x192.png"],
-    site: "https://beautiful-gate.vercel.app/",
+    site: siteURL,
   },
+  category: 'church',
+  appLinks: {
+    // ios: {
+    //   url: 'https://nextjs.org/ios',
+    //   app_store_id: 'app_store_id',
+    // },
+    // android: {
+    //   package: 'com.example.android/package',
+    //   app_name: 'app_name_android',
+    // },
+    web: {
+      url: siteURL,
+      should_fallback: true,
+    },
+  },
+
   robots: {
-    index: true,
+    index: false,
     follow: true,
     nocache: true,
     googleBot: {
       index: true,
-      follow: true,
+      follow: false,
       noimageindex: true,
       "max-video-preview": -1,
       "max-image-preview": "large",
---
+++ src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseURL = `https://beautiful-gate.vercel.app/`
+    return [
+        {
+            url: baseURL,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: `${baseURL}about`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: `${baseURL}events`,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+    ]
+}
