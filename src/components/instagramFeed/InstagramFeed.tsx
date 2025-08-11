125c7ea766d4b495cd8166ab152577d9b705fb4b
lightpc90 <afoageektech@gmail.com>
2025-08-11 01:27:24 +0100

    changed the IG url to instagram from facebook

--- src/components/instagramFeed/InstagramFeed.tsx
+++ src/components/instagramFeed/InstagramFeed.tsx
@@ -3,103 +3,103 @@ import Link from "next/link";
 import React from "react";
 
 type MediaItem = {
-  id: string;
-  media_type: "IMAGE" | "VIDEO";
-  media_url: string;
-  thumbnail_url?: string;
+	id: string;
+	media_type: "IMAGE" | "VIDEO";
+	media_url: string;
+	thumbnail_url?: string;
 };
 
 type InstagramPost = {
-  id: string;
-  caption?: string;
-  media_url?: string;
-  thumbnail_url?: string;
-  permalink: string;
-  media_type: "IMAGE" | "VIDEO" | "CAROUSEL_ALBUM";
-  children?: { data: MediaItem[] };
+	id: string;
+	caption?: string;
+	media_url?: string;
+	thumbnail_url?: string;
+	permalink: string;
+	media_type: "IMAGE" | "VIDEO" | "CAROUSEL_ALBUM";
+	children?: { data: MediaItem[] };
 };
 const InstagramFeed = async () => {
-  const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
-  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,thumbnail_url,media_type&access_token=${accessToken}`;
-  try {
-    const res = await fetch(url, { next: { revalidate: 3600 } }); //revalidat every 1 hour
-    const data = await res.json();
+	const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
+	console.log("access token: ", accessToken);
+	const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,thumbnail_url,media_type&access_token=${accessToken}`;
+	try {
+		const res = await fetch(url, { next: { revalidate: 3600 } }); //revalidat every 1 hour
+		const data = await res.json();
 
-    if (!res.ok || data?.data.length < 1) {
-      throw new Error("Failed to fetch instagram posts");
-    }
-    // filter only the 9 most recent posts
-    const posts: InstagramPost[] = data.data.slice(0, 9);
-    // console.log("instagram data fetched...", posts);
-    // Helper function to get the media to display
-    const getDisplayMedia = (post: InstagramPost) => {
-      if (post.media_type === "CAROUSEL_ALBUM") {
-        if (post.media_url) {
-          return post.media_url;
-        }
-        // If it's a carousel, use the first item's thumbnail_url or media_url
-        if (post.thumbnail_url) {
-          return post.thumbnail_url;
-        }
-        else return ''
-      }
+		if (!res.ok || data?.data.length < 1) {
+			throw new Error("Failed to fetch instagram posts");
+		}
+		// filter only the 9 most recent posts
+		const posts: InstagramPost[] = data.data.slice(0, 9);
+		// console.log("instagram data fetched...", posts);
+		// Helper function to get the media to display
+		const getDisplayMedia = (post: InstagramPost) => {
+			if (post.media_type === "CAROUSEL_ALBUM") {
+				if (post.media_url) {
+					return post.media_url;
+				}
+				// If it's a carousel, use the first item's thumbnail_url or media_url
+				if (post.thumbnail_url) {
+					return post.thumbnail_url;
+				} else return "";
+			}
 
-      // For single video posts, use the thumbnail_url
-      if (post.media_type === "VIDEO" && post.thumbnail_url) {
-        return post.thumbnail_url;
-      }
+			// For single video posts, use the thumbnail_url
+			if (post.media_type === "VIDEO" && post.thumbnail_url) {
+				return post.thumbnail_url;
+			}
 
-      // For image posts, use the media_url
-      if (post.media_type === "IMAGE") {
-        return post.media_url;
-      }
+			// For image posts, use the media_url
+			if (post.media_type === "IMAGE") {
+				return post.media_url;
+			}
 
-      // Default fallback if no media is found
-      return "";
-    };
-    return (
-      <div className="grid grid-cols-3 gap-2">
-        {posts.map((post) => {
-          const mediaUrl = getDisplayMedia(post);
-          return (
-            <Link
-              key={post.id}
-              href={post.permalink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                src={mediaUrl || ""}
-                alt={post.caption || `Instagram post`}
-                width={400}
-                height={400}
-                className="w-full h-full rounded-md shadow-md"
-              />
-            </Link>
-          );
-        })}
-      </div>
-    );
-  } catch (e) {
-    console.error(e);
-    return (
-      <div className="flex flex-col w-full h-full items-center justify-center lg:text-xl font-bold p-6">
-        <p>Failed to load Instagram Posts. Please try again later</p>
-        <div className="grid grid-cols-3 gap-2 w-full">
-          {Array(9)
-            .fill(null)
-            .map((_, index) => (
-              <div
-                key={index}
-                className="w-full h-[90px] lg:h-[260px] bg-gray-600 rounded-md
+			// Default fallback if no media is found
+			return "";
+		};
+		return (
+			<div className="grid grid-cols-3 gap-2">
+				{posts.map(post => {
+					const mediaUrl = getDisplayMedia(post);
+					return (
+						<Link
+							key={post.id}
+							href={post.permalink}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image
+								src={mediaUrl || ""}
+								alt={post.caption || `Instagram post`}
+								width={400}
+								height={400}
+								className="w-full h-full rounded-md shadow-md"
+							/>
+						</Link>
+					);
+				})}
+			</div>
+		);
+	} catch (e) {
+		console.error(e);
+		return (
+			<div className="flex flex-col w-full h-full items-center justify-center lg:text-xl font-bold p-6">
+				<p>Failed to load Instagram Posts. Please try again later</p>
+				<div className="grid grid-cols-3 gap-2 w-full">
+					{Array(9)
+						.fill(null)
+						.map((_, index) => (
+							<div
+								key={index}
+								className="w-full h-[90px] lg:h-[260px] bg-gray-600 rounded-md
         hover:shadow-lg hover:ring ring-rose-800 cursor-pointer 
         "
-              ></div>
-            ))}
-        </div>
-      </div>
-    );
-  }
+							></div>
+						))}
+				</div>
+			</div>
+		);
+	}
 };
 
 export default InstagramFeed;
--- src/components/instagramFeed/InstagramProfile.tsx
+++ src/components/instagramFeed/InstagramProfile.tsx
@@ -2,74 +2,75 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface InstagramAccountInfo {
-  id: string;
-  username: string;
-  profile_picture_url: string;
-  followers_count: number;
-  follows_count: number;
+	id: string;
+	username: string;
+	profile_picture_url: string;
+	followers_count: number;
+	follows_count: number;
 }
 
-
-
 const InstagramProfile: React.FC = async () => {
-  const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN; 
-  const url = `https://graph.facebook.com/me?fields=id,username,profile_picture_url,followers_count,follows_count&access_token=${accessToken}`;
+	const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
+	console.log("access token used in IG profile: ", accessToken);
+	const url = `https://graph.instagram.com/me?fields=id,username,profile_picture_url,followers_count,follows_count&access_token=${accessToken}`;
 
-  // Fetch account information
-  try {
-    const response = await fetch(url, { next: { revalidate: 3600 } });
-    if (!response.ok) {
-      const errorText = await response.text();
-      console.error("Failed to fetch:", response.status, errorText);
-      throw new Error("Failed to fetch Instagram account info");
-    }
-    const profileData = await response.json();
-    console.log("profile data objects: ", profileData)
-    return (
-      <div className="w-full bg-zinc-950 text-white flex flex-col p-4 rounded-md shadow-md mb-2 overflow-hidden relative">
-        <div className="absolute h-[300px] w-[300px] rounded-full top-[-70%] lg:top-[-70%] right-[-50%] lg:right-[-10%] bg-zinc-900 z-[1] cyclingReverse"></div>
-        <div className="flex gap-2 items-center z-[3]">
-          <Image
-            src={profileData.profile_picture_url}
-            alt={`${profileData.username}'s profile`}
-            height={200}
-            width={200}
-            style={{ borderRadius: "50%", width: "40px", height: "40px" }}
-          />
-          <h2>{profileData?.username}</h2>
-        </div>
-        <div className="flex gap-4 items-center my-3 z-[3]">
-          <p>
-            Followers: <span className="font-bold">{profileData.followers_count}</span>
-          </p>
-          <p>
-            Following: <span className="font-bold">{profileData.follows_count}</span>
-          </p>
-        </div>
+	// Fetch account information
+	try {
+		const response = await fetch(url, { next: { revalidate: 3600 } });
+		if (!response.ok) {
+			const errorText = await response.text();
+			console.error("Failed to fetch:", response.status, errorText);
+			throw new Error("Failed to fetch Instagram account info");
+		}
+		const profileData = await response.json();
+		console.log("profile data objects: ", profileData);
+		return (
+			<div className="w-full bg-zinc-950 text-white flex flex-col p-4 rounded-md shadow-md mb-2 overflow-hidden relative">
+				<div className="absolute h-[300px] w-[300px] rounded-full top-[-70%] lg:top-[-70%] right-[-50%] lg:right-[-10%] bg-zinc-900 z-[1] cyclingReverse"></div>
+				<div className="flex gap-2 items-center z-[3]">
+					<Image
+						src={profileData.profile_picture_url}
+						alt={`${profileData.username}'s profile`}
+						height={200}
+						width={200}
+						style={{ borderRadius: "50%", width: "40px", height: "40px" }}
+					/>
+					<h2>{profileData?.username}</h2>
+				</div>
+				<div className="flex gap-4 items-center my-3 z-[3]">
+					<p>
+						Followers:{" "}
+						<span className="font-bold">{profileData.followers_count}</span>
+					</p>
+					<p>
+						Following:{" "}
+						<span className="font-bold">{profileData.follows_count}</span>
+					</p>
+				</div>
 
-        {/* Follow Button */}
-        <Link
-          href={`https://www.instagram.com/${profileData.username}/`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="w-fit font-bold bg-blue-800 shadow-md z-[3] "
-          style={{
-            padding: "10px 50px",
-            color: "#fff",
-            textDecoration: "none",
-            borderRadius: "5px",
-            display: "inline-block",
-            marginTop: "10px",
-          }}
-        >
-          Follow
-        </Link>
-      </div>
-    );
-  } catch (error) {
-    console.error("Error fetching Instagram account info:", error);
-    return <div>Error fetching Instagram account info</div>;
-  }
+				{/* Follow Button */}
+				<Link
+					href={`https://www.instagram.com/${profileData.username}/`}
+					target="_blank"
+					rel="noopener noreferrer"
+					className="w-fit font-bold bg-blue-800 shadow-md z-[3] "
+					style={{
+						padding: "10px 50px",
+						color: "#fff",
+						textDecoration: "none",
+						borderRadius: "5px",
+						display: "inline-block",
+						marginTop: "10px",
+					}}
+				>
+					Follow
+				</Link>
+			</div>
+		);
+	} catch (error) {
+		console.error("Error fetching Instagram account info:", error);
+		return <div>Error fetching Instagram account info</div>;
+	}
 };
 
 export default InstagramProfile;
